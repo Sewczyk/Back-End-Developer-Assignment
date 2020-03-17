ee6b07daaa9e6a75fd6a29fbe7e0f5f8ee03c468
Sebastian Magnuszewski <sewycz@interia.eu>
2020-03-17 22:10:38 +0100

    Initial Project Commit

---
+++ src/main/java/com/polsource/BackEndDeveloperAssignment/Main.java
@@ -0,0 +1,13 @@
+package com.polsource.BackEndDeveloperAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
