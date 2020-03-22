d7b8b3df3909d2704fe521c5cdf2a64b9556ff61
Sebastian Magnuszewski <magnuszewski.seb@gmail.com>
2020-03-22 20:44:42 +0100

    FileDB

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,5 +6,3 @@ create table if not exists notes(
     created smalldatetime,
     modified smalldatetime
 );
-
-insert into notes (UUID, noteID, title, content) values ('123e4567-e89b-12d3-a456-426655440000', 1 , 'welp', 'testmessage');
